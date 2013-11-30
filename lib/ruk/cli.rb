be365b4fabe781a1361b4f1813df47ef9c904e22
Howard Yeh <howard@metacircus.com>
2013-11-30 22:43:18 +0800

    got the CLI to work again

--- lib/ruk/cli.rb
+++ lib/ruk/cli.rb
@@ -1,7 +1,35 @@
 class Ruk::CLI
+  def self.options
+    opts = {}
+    OptionParser.new do |o|
+      o.banner = "Ruk #{Ruk::VERSION}"
+      o.separator "ruk [addr] expr"
+      o.on "-v", "--invert", "Invert the default matcher" do
+        opts[:invert] = true
+      end
+      o.on_tail "-h","--help","Show Help" do
+        puts o
+        exit
+      end
+    end
+  end
+
+  def self.run
+    opts = options.permute!
+    self.new(ARGV,opts).run
+  end
+
+  def initialize(args,opts={})
+    @options = opts
+  end
+
   def run
-    options.permute!
+    # Ruk::CLI.options.permute!
     args = ARGV
+    if args.empty?
+      puts "no expression given"
+      exit 99
+    end
     evaluator = Ruk::Eval.build *args
     input = $stdin
     output = $stdout
@@ -10,17 +38,4 @@ class Ruk::CLI
     processor.process
   end
 
-  def options
-    OptionParser.new do |o|
-      o.banner = "Ruk #{Ruk::VERSION}"
-      o.separator "ruk [addr] expr"
-      o.on "-v", "--invert", "Invert the default matcher" do
-        @invert = true
-      end
-      o.on_tail "-h","--help","Show Help" do
-        puts o
-        exit
-      end
-    end
-  end
 end
